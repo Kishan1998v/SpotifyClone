7338ac16a4984e3492f27487c32c0bfaa35dd696
kishan vishwakarma <kishan.1998.v@gmail.com>
2022-12-20 20:15:24 +0530

    SpotifyClone (0.0.2) : Tailwind Css Style setup is completed

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+//Remember When tou use '.js' the Appp will throught the Error :
+//So use the '.cjs' filetype and This will solve the error;
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //what we saying to tailwind that ,Go chechk at "src" folder for all the files
+  //
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
